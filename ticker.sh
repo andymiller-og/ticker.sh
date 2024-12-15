d9b0b739c6fb7120824014d7da0873ab95f1af8c
andymiller-og <anm5030@gmail.com>
2024-12-15 09:04:26 -0500

    Update to ensure the same order (much slower though)

--- ticker.sh
+++ ticker.sh
@@ -7,108 +7,129 @@ LC_NUMERIC=C
 : ${TMPDIR:=/tmp}
 SESSION_DIR="${TMPDIR%/}/ticker.sh-$(whoami)"
 COOKIE_FILE="${SESSION_DIR}/cookies.txt"
-CRUMB_FILE="${SESSION_DIR}/crumb.txt"
+API_ENDPOINT="https://query1.finance.yahoo.com/v8/finance/chart/"
+API_SUFFIX="?interval=1d"
 
-SYMBOLS=("$@")
+# Check if NO_COLOR is set to disable colorization
+if [ -z "$NO_COLOR" ]; then
+  : "${COLOR_GREEN:=$'\e[32m'}"
+  : "${COLOR_RED:=$'\e[31m'}"
+  : "${COLOR_RESET:=$'\e[00m'}"
+fi
+
+SYMBOLS=()
+DISPLAY_METALS=false
+
+# Parse options
+while getopts "g" opt; do
+  case ${opt} in
+    g )
+      DISPLAY_METALS=true
+      ;;
+    * )
+      echo "Usage: ./ticker.sh [-g] AAPL MSFT GOOG BTC-USD"
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+SYMBOLS+=("$@")
 
 if ! $(type jq > /dev/null 2>&1); then
   echo "'jq' is not in the PATH. (See: https://stedolan.github.io/jq/)"
   exit 1
 fi
 
-if [ -z "$SYMBOLS" ]; then
-  echo "Usage: ./ticker.sh AAPL MSFT GOOG BTC-USD"
-  exit
+if ! $(type bc > /dev/null 2>&1); then
+  echo "'bc' is not in the PATH. (See: https://www.gnu.org/software/bc/)"
+  exit 1
 fi
 
-FIELDS=(symbol marketState regularMarketPrice regularMarketChange regularMarketChangePercent \
-  preMarketPrice preMarketChange preMarketChangePercent postMarketPrice postMarketChange postMarketChangePercent)
-API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
-
-if [ -z "$NO_COLOR" ]; then
-  : "${COLOR_BOLD:=\e[1;37m}"
-  : "${COLOR_GREEN:=\e[32m}"
-  : "${COLOR_RED:=\e[31m}"
-  : "${COLOR_RESET:=\e[00m}"
+if [ -z "$SYMBOLS" ] && [ "$DISPLAY_METALS" = false ]; then
+  echo "Usage: ./ticker.sh [-g] AAPL MSFT GOOG BTC-USD"
+  exit 1
 fi
-
-symbols=$(IFS=,; echo "${SYMBOLS[*]}")
-fields=$(IFS=,; echo "${FIELDS[*]}")
 
 [ ! -d "$SESSION_DIR" ] && mkdir -m 700 "$SESSION_DIR"
 
 preflight () {
-  # rather than "finance", use the "fc" subdomain which doesn't redirect to a consent page
-  curl --silent --output /dev/null --cookie-jar "$COOKIE_FILE" "https://fc.yahoo.com" \
-  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
-  curl --silent -b "$COOKIE_FILE" "https://query1.finance.yahoo.com/v1/test/getcrumb" \
-    > "$CRUMB_FILE"
+  curl --silent --output /dev/null --cookie-jar "$COOKIE_FILE" "https://finance.yahoo.com" \
+    -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
 }
 
-fetch_quotes () {
-  curl --silent -b "$COOKIE_FILE" "$API_ENDPOINT&fields=$fields&symbols=$symbols&crumb=$(cat "$CRUMB_FILE")"
+fetch_chart () {
+  local symbol=$1
+  local url="${API_ENDPOINT}${symbol}${API_SUFFIX}"
+  curl --silent -b "$COOKIE_FILE" "$url"
 }
 
-[ ! -f "$COOKIE_FILE" -o ! -f "$CRUMB_FILE" ] && preflight
-results=$(fetch_quotes)
-if $(echo "$results" | grep -q '"code":"Unauthorized"'); then
-  preflight
-  results=$(fetch_quotes)
+[ ! -f "$COOKIE_FILE" ] && preflight
+
+fetch_metal_prices () {
+  local gold_symbol="GC=F"
+  local silver_symbol="SI=F"
+  local platinum_symbol="PL=F"
+
+  local gold_price=$(fetch_chart "$gold_symbol" | jq -r '.chart.result[0].meta.regularMarketPrice')
+  local silver_price=$(fetch_chart "$silver_symbol" | jq -r '.chart.result[0].meta.regularMarketPrice')
+  local platinum_price=$(fetch_chart "$platinum_symbol" | jq -r '.chart.result[0].meta.regularMarketPrice')
+
+  local gold_silver_ratio=$(awk -v gold="$gold_price" -v silver="$silver_price" 'BEGIN {printf "%.2f", gold / silver}')
+
+  # Define colors
+  local COLOR_YELLOW=$'\e[33m'
+  local COLOR_SILVER=$'\e[37m'
+  local COLOR_LIGHT_GREY=$'\e[249m'
+  local COLOR_BRIGHT_PURPLE=$'\e[35;1m'
+  local COLOR_RESET=$'\e[0m'
+
+  echo "Precious Metal Spot Prices:"
+  echo "---------------------------"
+  printf "${COLOR_YELLOW}Gold Spot:     $%.2f /OZ${COLOR_RESET}\n" "$gold_price"
+  printf "${COLOR_SILVER}Silver Spot:   $%.2f /OZ${COLOR_RESET}\n" "$silver_price"
+  printf "${COLOR_LIGHT_GREY}Platinum Spot: $%.2f /OZ${COLOR_RESET}\n" "$platinum_price"
+  printf "${COLOR_BRIGHT_PURPLE}Gold/Silver Ratio: %.2f${COLOR_RESET}\n" "$gold_silver_ratio"
+  echo ""
+}
+
+if [ "$DISPLAY_METALS" = true ]; then
+  fetch_metal_prices
 fi
 
-results=$(echo $results | jq '.quoteResponse .result')
+# Create a temporary file to store results
+TEMP_FILE=$(mktemp)
 
-query () {
-  echo $results | jq -r ".[] | select(.symbol == \"$1\") | .$2"
-}
+for symbol in "${SYMBOLS[@]}"; do
+  results=$(fetch_chart "$symbol")
 
-for symbol in $(IFS=' '; echo "${SYMBOLS[*]}" | tr '[:lower:]' '[:upper:]'); do
-  marketState="$(query $symbol 'marketState')"
+  currentPrice=$(echo "$results" | jq -r '.chart.result[0].meta.regularMarketPrice')
+  previousClose=$(echo "$results" | jq -r '.chart.result[0].meta.chartPreviousClose')
+  currency=$(echo "$results" | jq -r '.chart.result[0].meta.currency')
+  symbol=$(echo "$results" | jq -r '.chart.result[0].meta.symbol')
 
-  if [ -z $marketState ]; then
-    printf 'No results for symbol "%s"\n' $symbol
-    continue
+  [ "$previousClose" = "null" ] && previousClose="1.0"
+
+  priceChange=$(awk -v currentPrice="$currentPrice" -v previousClose="$previousClose" 'BEGIN {printf "%.2f", currentPrice - previousClose}')
+  percentChange=$(awk -v currentPrice="$currentPrice" -v previousClose="$previousClose" 'BEGIN {printf "%.2f", ((currentPrice - previousClose) / previousClose) * 100}')
+
+  if (( $(echo "$priceChange >= 0" | bc -l) )); then
+    color="$COLOR_GREEN"
+  elif (( $(echo "$priceChange < 0" | bc -l) )); then
+    color="$COLOR_RED"
   fi
 
-  preMarketChange="$(query $symbol 'preMarketChange')"
-  postMarketChange="$(query $symbol 'postMarketChange')"
-
-  if [ $marketState = "PRE" ] \
-    && [ $preMarketChange != "0" ] \
-    && [ $preMarketChange != "null" ]; then
-    nonRegularMarketSign='*'
-    price=$(query $symbol 'preMarketPrice')
-    diff=$preMarketChange
-    percent=$(query $symbol 'preMarketChangePercent')
-  elif [ $marketState != "REGULAR" ] \
-    && [ $postMarketChange != "0" ] \
-    && [ $postMarketChange != "null" ]; then
-    nonRegularMarketSign='*'
-    price=$(query $symbol 'postMarketPrice')
-    diff=$postMarketChange
-    percent=$(query $symbol 'postMarketChangePercent')
+  if [ -z "$NO_COLOR" ]; then
+    printf "%s%-10s%8.2f%10.2f%8s%6.2f%%%s\n" \
+      "$color" "$symbol" \
+      "$currentPrice" "$priceChange" "$color" "$percentChange" \
+      "$COLOR_RESET"
   else
-    nonRegularMarketSign=''
-    price=$(query $symbol 'regularMarketPrice')
-    diff=$(query $symbol 'regularMarketChange')
-    percent=$(query $symbol 'regularMarketChangePercent')
-  fi
-
-  # see https://github.com/pstadler/ticker.sh/issues/40
-  [ "$diff" = "null" ] && diff="0.0"
-  [ "$percent" = "null" ] && percent="0.0"
-
-  if [ "$diff" = "0" ] || [ "$diff" = "0.0" ]; then
-    color=
-  elif ( echo "$diff" | grep -q ^- ); then
-    color=$COLOR_RED
-  else
-    color=$COLOR_GREEN
-  fi
-
-  if [ "$price" != "null" ]; then
-    printf "%-10s$COLOR_BOLD%8.2f$COLOR_RESET" $symbol $price
-    printf "$color%10.2f%12s$COLOR_RESET" $diff $(printf "(%.2f%%)" $percent)
-    printf " %s\n" "$nonRegularMarketSign"
+    printf "%-10s%8.2f%10.2f%9.2f%%\n" \
+      "$symbol" \
+      "$currentPrice" "$priceChange" "$percentChange"
   fi
 done
+
+# Clean up
+rm -f "$TEMP_FILE"
